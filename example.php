fc54aae62f287649530757fb03a4aaaff1526896
Gareth Midwood <gareth.midwood@creode.co.uk>
2019-02-23 00:35:49 +0000

    Add further messages to example file

--- example.php
+++ example.php
@@ -4,13 +4,13 @@ require_once('vendor/autoload.php');
 include_once('config.example.php');
 
 // these params would be defined in the example.config.php file
-$ticketingSystem = new GarethMidwood\CodebaseHQ\CodebaseHQAccount(
+$codebaseHQ = new GarethMidwood\CodebaseHQ\CodebaseHQAccount(
     $apiUser,
     $apiKey,
     $apiHost
 );
 
-$projects = $ticketingSystem->projects();
+$projects = $codebaseHQ->projects();
 
 echo 'retrieved ' . $projects->getCount() . ' total projects' . PHP_EOL;
 
@@ -22,9 +22,12 @@ $activeProjects = $projects->getActive();
 
 echo 'retrieved ' . $activeProjects->getCount() . ' active projects' . PHP_EOL;
 
+$searchedProjects = $activeProjects->searchByName('Creode', true);
+
+echo 'retrieved ' . $searchedProjects->getCount() . ' active searched projects' . PHP_EOL;
+
 echo '---' . PHP_EOL;
 
-$searchedProjects = $activeProjects->searchByName('Creode');
 
 // populate the tickets for each project
 foreach($searchedProjects as $project) {
@@ -32,7 +35,7 @@ foreach($searchedProjects as $project) {
     $moreResultsToRetrieve = true;
 
     while ($moreResultsToRetrieve) {
-        $moreResultsToRetrieve = $ticketingSystem->tickets($project, $pageNo);
+        $moreResultsToRetrieve = $codebaseHQ->tickets($project, $pageNo);
         $pageNo++;
     }
 }
@@ -40,6 +43,11 @@ foreach($searchedProjects as $project) {
 
 foreach($searchedProjects as $project) {
     $tickets = $project->getTickets();
+
+    if ($tickets->getCount() == 0) {
+        echo $project->getName() . ' has no tickets. Have you populated them?' . PHP_EOL;
+        continue;
+    }
 
     echo $tickets->getCount() . ' tickets in ' . $project->getName() . PHP_EOL;
 
